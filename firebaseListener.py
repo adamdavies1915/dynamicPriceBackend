fe6ba7b3dad217b4fa3393ca7c1c70b3461bb269
Adam Davies <adam.davies@pepperhq.com>
2019-06-16 11:51:06 +0100

    pricing backend

--- firebaseListener.py
+++ firebaseListener.py
@@ -3,6 +3,8 @@ import pandas as pd
 import math
 import schedule
 import time
+from twilio.rest import Client
+
 
 def runPricing():
   rooms = db.child('roomDetails').get().val()
@@ -14,7 +16,7 @@ def runPricing():
 def getTimeToBookingWeighting(bookingDatetime):
   differenceInHours = (bookingDatetime - pd.Timestamp.now()).total_seconds()/3600
   if differenceInHours < 168:
-    return 2.92 + (3.44*math.log(differenceInHours))
+    return 2.92 + (3.44 * math.log(differenceInHours))
   else:
     return 1
 
@@ -35,6 +37,23 @@ def calculatePriceOfMeetingRoom(room):
       roomPrices[datetime.strftime("%m-%d-%Y:%H:%M:%S")] = round((1 * (room["capacity"]/2.5) * getTimeToBookingWeighting(datetime)), 2)
     return roomPrices
 
+
+def stream_handler(message):
+  try:
+    print("change")
+    print("sending sms")
+    message = client.messages.create(
+    to="+447419345365",
+    from_="+441356707052",
+    body="Hello the booking you're watching: "+message["data"]+" has become available")
+
+    print(message["data"]) # {'title': 'Pyrebase', "body": "etc..."}
+    print(message.sid)
+  except:
+    pass
+
+
+###Firebase config
 config = {
     "apiKey": "AIzaSyASUKH1z9HYxtqIWhbsufQgWK0E28I8zoE",
     "authDomain": "dynamicprice-36096.firebaseapp.com",
@@ -47,8 +66,19 @@ firebase = pyrebase.initialize_app(config)
 
 db = firebase.database()
 
-schedule.every(1).minutes.do(runPricing)
 
-while(True):
-  schedule.run_pending()
-  time.sleep(1)
+###Twilio config
+
+
+account_sid = "AC7862d1c7307e0ebcf16075a46840f0f2"
+auth_token = "fc0342180b028509f1085cd58f380304"
+client = Client(account_sid, auth_token)
+
+
+my_stream = db.child("roomBookings").stream(stream_handler)
+
+# schedule.every(1).minutes.do(runPricing)
+
+# while(True):
+#   schedule.run_pending()
+#   time.sleep(1)
